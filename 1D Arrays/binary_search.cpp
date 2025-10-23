018fe97fb6d6f879c121bd5a082e043d18b3fa1d
Daksh Jand <dakshjand@192.168.1.6>
2025-10-23 22:42:49 +0530

    Whole code

--- 1D Arrays/binary_search.cpp
+++ 1D Arrays/binary_search.cpp
@@ -1,5 +1,32 @@
+//array should be sorted
+//for this we locate the index by using mid terms.
+
+
 #include <iostream>
 using namespace std;
-int main(){
-    
+
+int main() {
+    int arr[5] = {1,2,3,4,7};  
+    int start = 0;
+    int end = 4;
+    int n = 7;
+
+    while(start <= end){
+        int mid = (start + end)/2;   
+        if(arr[mid] == n){
+            cout <<mid << endl;
+            break;
+        }
+        else if(n > arr[mid]){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+    }
+
+    return 0;
 }
+
+
+
